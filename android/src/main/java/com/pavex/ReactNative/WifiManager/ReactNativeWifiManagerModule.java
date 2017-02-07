96cd14fffdccc45e9f675fcba0813c88b0094c35
pavex <pavex@ines.cz>
2017-02-07 17:54:32 +0100

    Beta version of Wifi manager for React Native applications

---
+++ android/src/main/java/com/pavex/ReactNative/WifiManager/ReactNativeWifiManagerModule.java
@@ -0,0 +1,122 @@
+/**
+ * @author Pavex <pavex@ines.cz>
+ * Many thanks for Ray Gläske for base code of getWifiList and startScan
+ *   from (https://github.com/Haargeeel/react-native-wifi-checker)
+ */
+package com.pavex.ReactNative.WifiManager;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+
+
+
+
+
+/**
+ */
+public class ReactNativeWifiManagerModule extends ReactContextBaseJavaModule {
+
+
+	public ReactNativeWifiManagerModule(ReactApplicationContext reactContext) {
+		super(reactContext);
+	}
+
+
+
+
+
+	@Override
+	public String getName() {
+		return "ReactNativeWifiManager";
+	}
+
+
+
+
+
+	@ReactMethod
+	public void getWifiList(Promise promise) {
+		StringBuilder response = new StringBuilder();
+		WifiManager wifiManager = (WifiManager) getReactApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+		WritableArray wifiList = Arguments.createArray();
+		for (int i = 0; i < wifiManager.getScanResults().size(); i++) {
+			ScanResult result = wifiManager.getScanResults().get(i);
+			WritableMap map = Arguments.createMap();
+			map.putString("SSID", result.SSID);
+			map.putString("BSSID", result.BSSID);
+			map.putString("capabilities", result.capabilities);
+			map.putInt("frequency", result.frequency);
+			map.putInt("level", WifiManager.calculateSignalLevel(result.level, 100));
+			map.putDouble("timestamp", result.timestamp);
+//			map.putString("operatorFriendlyName", getString(result.operatorFriendlyName));
+//			map.putString("venueName", getString(result.venueName));
+//			map.putInt("centerFreq0", result.centerFreq0);
+//			map.putInt("centerFreq1", result.centerFreq1);
+//			map.putInt("channelWidth", result.channelWidth);
+			wifiList.pushMap(map);
+		}
+		if (wifiList.size() > 0) {
+			promise.resolve(wifiList);
+		}
+		else {
+			promise.reject("No found wifi.");
+		}
+	}
+
+
+
+
+
+	@ReactMethod
+	public void startScan(Promise promise) {
+		WifiManager wifiManager = (WifiManager) getReactApplicationContext().getSystemService(Context.WIFI_SERVICE);
+		if (wifiManager.startScan()) {
+			promise.resolve(true);
+		}
+		else {
+			promise.reject("Can not scan for wifi.");
+		}
+	}
+
+
+
+
+
+	@ReactMethod
+	public void isEnabled(Promise promise) {
+		WifiManager wifiManager = (WifiManager) getReactApplicationContext().getSystemService(Context.WIFI_SERVICE);
+		if (wifiManager.isWifiEnabled()) {
+			promise.resolve(true);
+		}
+		else {
+			promise.reject("Wifi is disable.");
+		}
+	}
+
+
+
+
+
+	@ReactMethod
+	public void setEnabled(Boolean enabled, Promise promise) {
+		WifiManager wifiManager = (WifiManager) getReactApplicationContext().getSystemService(Context.WIFI_SERVICE);
+		if (wifiManager.setWifiEnabled(enabled)) {
+			promise.resolve(true);
+		}
+		else {
+			promise.reject("Can not change wifi state.");
+		}
+	}
+
+
+}
---
+++ android/src/main/java/com/pavex/ReactNative/WifiManager/ReactNativeWifiManagerPackage.java
@@ -0,0 +1,52 @@
+/**
+ * @author Pavex <pavex@ines.cz>
+ */
+package com.pavex.ReactNative.WifiManager;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+
+
+
+/**
+ */
+public class ReactNativeWifiManagerPackage implements ReactPackage {
+
+
+	@Override
+	public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+		List<NativeModule> modules = new ArrayList<>();
+		modules.add(new ReactNativeWifiManagerModule(reactContext));
+		return modules;
+	}
+
+
+
+
+
+	@Override
+	public List<Class<? extends JavaScriptModule>> createJSModules() {
+		return Collections.emptyList();
+	}
+
+
+
+
+
+	@Override
+	public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+		return Arrays.<ViewManager>asList();
+	}
+
+
+}
